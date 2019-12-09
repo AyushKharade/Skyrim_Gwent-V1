9fec13a17148231dad396face3a685a39f3e17e5
AyushKharade <ayush.kharade999@gmail.com>
2019-12-09 15:18:55 -0500

    adjusted prefab card scale

---
+++ Skyrim Gwent V1 2020/Assets/Scripts/Gameplay/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    GameObject raycastTarget;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCameraRaycast();  
+    }
+
+
+    void GetCameraRaycast()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Debug.Log("Hovering on: " + hit.transform.name);
+        }
+    }
+}
