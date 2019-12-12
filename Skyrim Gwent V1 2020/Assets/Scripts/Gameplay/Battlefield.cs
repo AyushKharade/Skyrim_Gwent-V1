71e4a75a57582176bb1db0f4081ce620aaa1255f
AyushKharade <ayush.kharade999@gmail.com>
2019-12-11 22:22:26 -0500

    refactored player.cs with linked list

---
+++ Skyrim Gwent V1 2020/Assets/Scripts/Gameplay/Battlefield.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * This script will be used to take care of the 3 battle zones for players. 
+ * This has 3 lists for storing all cards, and rearrange functions for properly displaying cards.
+ * 2 instantiations of this script will be present in the game
+ * this also takes care of UI
+ * 
+ * In future, also add discard pile and starting hand lists
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//variables
+
+public class Battlefield : MonoBehaviour
+{
+    public string PlayerName;
+    [Range(1, 2)]
+    public int PlayerID;
+
+    [HideInInspector] public LinkedList<GameObject> frontline;
+    [HideInInspector] public LinkedList<GameObject> vantage;
+    [HideInInspector] public LinkedList<GameObject> shadow;
+    [HideInInspector] public LinkedList<GameObject> discardpile;
+
+
+    public int frontlineScore=0;
+    public int vantagelineScore=0;
+    public int shadowScore=0;
+    public int totalScore=0;
+
+    //UI
+    public Text frontlineScoreUI;
+    Text vantagelineScoreUI;
+    Text shadowScoreUI;
+    Text totalScoreUI;
+
+
+    // card position adjustment parameters
+    float x;
+    float y;
+
+    private void Start()
+    {
+        frontline = new LinkedList<GameObject>();
+        vantage = new LinkedList<GameObject>();
+        shadow = new LinkedList<GameObject>();
+        discardpile = new LinkedList<GameObject>();
+    }
+
+    private void Update()
+    {
+        UpdateScoreUI();
+        Debug.Log("Units in Frontline: " + frontline.Count);
+
+    }
+
+    public void UpdateScoreUI()
+    {
+        frontlineScoreUI.text = "" + frontlineScore;
+    }
+
+
+    // public add cards to any fields
+    public void AddUnitToFrontline(GameObject UnitCard)
+    {
+        Debug.Log("Unit Received: "+ UnitCard.GetComponent<Card>().info.name);
+        UnitCard.transform.Translate(new Vector3(3, 0, 0));
+
+        frontline.AddLast(UnitCard);
+        frontlineScore += UnitCard.GetComponent<Card>().info.strength;
+        
+        
+        //adjust scores according to buffs or debuffs (Phase 2)
+
+
+        // update positions
+        
+    }
+
+}
--- Skyrim Gwent V1 2020/Assets/Scripts/Gameplay/Player.cs
+++ Skyrim Gwent V1 2020/Assets/Scripts/Gameplay/Player.cs
@@ -1,43 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-
-
     GameObject raycastTarget;
 
-    public GameObject P1Frontlines;
-    public GameObject P1Vantage;
-    public GameObject P1Shadow;
+    [Range(1,2)]
+    public int turn;
 
-    // ui
-    public Text P1Frontlines_Score;
-    public Text P1Vantage_Score;
-    public Text P1Shadow_Score;
-    public Text Total_Score;
+    public GameObject P1Battlefield;
+    public GameObject P2Battlefield;
 
+    Battlefield P1BFRef;
+    Battlefield P2BFRef;
+
+    // ui done from p1_battlefield
     
-
-
     void Start()
     {
-        
+        P1BFRef = P1Battlefield.GetComponent<Battlefield>();
+        P2BFRef = P2Battlefield.GetComponent<Battlefield>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         GetCameraRaycast();
         //DisplayRaycastTarget();
 
         //for scaling
-        if (raycastTarget != null)
-        {
-            raycastTarget.GetComponent<CardScaler>().underCursor=true;
-        }
+        CardScaling();
+
       
     }
 
@@ -45,19 +39,14 @@ public class Player : MonoBehaviour
     void GetCameraRaycast()
     {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
         if (hit.collider != null)
-        {
             raycastTarget = hit.collider.gameObject;
-        }
         else
-        {
             raycastTarget = null;
-        }
 
 
         // input
-        if (Input.GetMouseButton(0) && hit.collider != null)
+        if (Input.GetMouseButtonDown(0) && hit.collider != null)
         {
             //place card
             if(hit.collider.gameObject.GetComponent<Card>().GetCardStatus() == "Hand")
@@ -75,39 +64,54 @@ public class Player : MonoBehaviour
             //Debug.Log("No card:");
     }
 
+
+
+
     void DeployUnitCard(GameObject card)
     {
-        //delete, spawn as a child of respective zone.
-
-
-        //reset position:
-        //Vector3 newCardPos = new Vector3(0,0,0);
-        //card.GetComponent<RectTransform>().position = newCardPos; 
-        
-
-        //save a temp ref to script
         Card cardRef = card.GetComponent<Card>();
-
         if (cardRef.info.GetUnitType() == "Warrior")
         {
-            // place on frontline
-            Card temp=Instantiate(cardRef, P1Frontlines.transform);
-            temp.GetComponent<Card>().SetCardStatus("Deployed");
-            temp.GetComponent<CardScaler>().deployed = true;
-            
-            
-            // delete
-            Destroy(card.gameObject);
+            P1BFRef.AddUnitToFrontline(card);
         }
+
+
+
+
+
+
+
+
+
         else if (cardRef.info.GetUnitType() == "Mage" || cardRef.info.GetUnitType() == "Spellsword")
         {
             // place on Vantage
         }
+
+
+
+
+
+
+
         else if (cardRef.info.GetUnitType() == "Shadow")
         {
             // place on shadow
         }
        
+
+    }
+
+
+
+
+
+
+    // extra
+    void CardScaling()
+    {
+        if (raycastTarget != null)
+            raycastTarget.GetComponent<CardScaler>().underCursor = true;
 
     }
 }
