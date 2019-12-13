e47ad415fff42ec27c44e564b74e14564a0377bd
AyushKharade <ayush.kharade999@gmail.com>
2019-12-13 14:37:31 -0500

    created scripts for starting game

---
+++ Skyrim Gwent V1 2020/Assets/Scripts/Menu/GameStarter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ *  This is a dontdestroyonLoad class that stores information about which decks were selected and player names. 
+ *  Instance Destroyed manually when required 
+ */
+public class GameStarter : MonoBehaviour
+{
+
+    public GameObject P1Deck;
+    public GameObject P2Deck;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+
+    }
+}
