1ef07183110340edfe854ce59dd80c4a379b8756
AyushKharade <ayush.kharade999@gmail.com>
2019-12-13 11:42:40 -0500

    added graphic for main menu

---
+++ Skyrim Gwent V1 2020/Assets/Scripts/Menu/MenuCursor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCursor : MonoBehaviour
+{
+
+
+    public GameObject raycastTarget;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetCameraRaycast();
+        ButtonScaling();
+    }
+
+
+    void GetCameraRaycast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+            raycastTarget = hit.collider.gameObject;
+        else
+            raycastTarget = null;
+    }
+
+
+    void ButtonScaling()
+    {
+        if (raycastTarget != null)
+            raycastTarget.GetComponent<CardScaler>().underCursor = true;
+    }
+}
---
+++ Skyrim Gwent V1 2020/Assets/Scripts/Menu/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void PressPlay()
+    {
+        Debug.Log("Clicked Play");
+    }
+
+    public void PressOptions()
+    {
+        Debug.Log("Clicked Options");
+    }
+
+    public void PressDecks()
+    {
+        Debug.Log("Clicked Decks");
+    }
+
+    public void PressQuit()
+    {
+        Application.Quit();
+    }
+}
